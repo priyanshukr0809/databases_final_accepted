396e1364e315850e28c4edc56c436253137b85bc
PRIYANSHU KUMAR <priyanshu_kumar@epam.com>
2025-05-20 21:02:12 +0530

    Update main.tf

--- task06/modules/sql/main.tf
+++ task06/modules/sql/main.tf
@@ -1,6 +1,11 @@
+resource "random_password" "sql_password" {
+  length  = 16
+  special = true
+}
+
 resource "azurerm_mssql_server" "sql" {
   name                         = var.sql_server_name
-  resource_group_name          = var.resource_group_name
+  resource_group_name          = var.rg_name
   location                     = var.location
   version                      = "12.0"
   administrator_login          = var.sql_admin_username
@@ -8,41 +13,41 @@ resource "azurerm_mssql_server" "sql" {
   tags                         = var.tags
 }
 
-resource "azurerm_mssql_database" "sql_db" {
-  name      = var.sql_db_name
-  server_id = azurerm_mssql_server.sql.id
-  sku_name  = var.sql_db_sku
-  tags      = var.tags
-}
-
-resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
-  name             = "AllowAllAzureIps"
+resource "azurerm_mssql_firewall_rule" "azure_services" {
+  name             = "AllowAzureServices"
   server_id        = azurerm_mssql_server.sql.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
 }
 
-resource "azurerm_mssql_firewall_rule" "allow_verification_ip" {
-  name             = var.firewall_rule_name
+resource "azurerm_mssql_firewall_rule" "allow_my_ip" {
+  name             = var.sql_fwr_name
   server_id        = azurerm_mssql_server.sql.id
   start_ip_address = var.allowed_ip_address
   end_ip_address   = var.allowed_ip_address
 }
 
-resource "random_password" "sql_password" {
-  length           = 16
-  special          = true
-  override_special = "!@#$%"
+resource "azurerm_mssql_database" "sql_db" {
+  name      = var.sql_db_name
+  server_id = azurerm_mssql_server.sql.id
+  sku_name  = var.sql_sku
+  tags      = var.tags
 }
 
-resource "azurerm_key_vault_secret" "sql_admin_name" {
-  name         = var.secret_name_admin
+resource "azurerm_key_vault_secret" "admin_username" {
+  name         = var.sql_admin_secret_name
   value        = var.sql_admin_username
-  key_vault_id = var.key_vault_id
+  key_vault_id = data.azurerm_key_vault.existing.id
 }
 
-resource "azurerm_key_vault_secret" "sql_admin_password" {
-  name         = var.secret_name_password
+resource "azurerm_key_vault_secret" "admin_password" {
+  name         = var.sql_admin_secret_password
   value        = random_password.sql_password.result
-  key_vault_id = var.key_vault_id
+  key_vault_id = data.azurerm_key_vault.existing.id
+  depends_on   = [random_password.sql_password]
+}
+
+data "azurerm_key_vault" "existing" {
+  name                = var.kv_name
+  resource_group_name = var.kv_rg_name
 }
