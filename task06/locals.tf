7f1bcd4bea11e3bb5a92469461d973b8cf89d94c
PRIYANSHU KUMAR <priyanshu_kumar@epam.com>
2025-05-20 20:59:24 +0530

    Update locals.tf

--- task06/locals.tf
+++ task06/locals.tf
@@ -1,8 +1,11 @@
 locals {
-  name_prefix     = var.name_prefix
-  rg_name         = format("%s-rg", local.name_prefix)
-  sql_server_name = format("%s-sql", local.name_prefix)
-  sql_db_name     = format("%s-sql-db", local.name_prefix)
-  asp_name        = format("%s-asp", local.name_prefix)
-  app_name        = format("%s-app", local.name_prefix)
+  tags = {
+    Creator = var.student_email
+  }
+
+  rg_name         = format("%s-rg", var.resources_name_prefix)
+  sql_server_name = format("%s-sql", var.resources_name_prefix)
+  sql_db_name     = format("%s-sql-db", var.resources_name_prefix)
+  asp_name        = format("%s-asp", var.resources_name_prefix)
+  app_name        = format("%s-app", var.resources_name_prefix)
 }
