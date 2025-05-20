a9db77415dad7deaca11951646b4d677ce07658f
PRIYANSHU KUMAR <priyanshu_kumar@epam.com>
2025-05-20 21:05:16 +0530

    Update variables.tf

--- task06/modules/webapp/variables.tf
+++ task06/modules/webapp/variables.tf
@@ -1,41 +1,40 @@
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group where the App Service Plan and Web App will be deployed."
-}
-
 variable "location" {
   type        = string
-  description = "The Azure region where the App Service Plan and Web App will be deployed (e.g., 'westus2')."
+  description = "Azure region where the App Service and related resources will be deployed"
+}
+
+variable "rg_name" {
+  type        = string
+  description = "Name of the resource group in which the App Service resources will be created"
 }
 
 variable "asp_name" {
   type        = string
-  description = "The name of the Azure App Service Plan."
-}
-
-variable "asp_sku" {
-  type        = string
-  description = "The SKU name for the Azure App Service Plan (e.g., 'P0v3')."
+  description = "Name of the Azure App Service Plan"
 }
 
 variable "app_name" {
   type        = string
-  description = "The name of the Azure Linux Web App."
+  description = "Name of the Azure Linux Web Application"
+}
+
+variable "asp_sku" {
+  type        = string
+  description = "The SKU tier for the App Service Plan (e.g., B1, S1)"
 }
 
 variable "dotnet_version" {
   type        = string
-  description = "The .NET version for the Linux Web App (e.g., '8.0')."
+  description = "The .NET Core version to be used by the Web Application (e.g., 6.0, 7.0)"
 }
 
-# Explicitly marked as sensitive to receive the secure SQL connection string
 variable "sql_connection_string" {
   type        = string
-  description = "The connection string for the Azure SQL Database to be used by the Web App."
+  description = "Connection string to the Azure SQL Database (ADO.NET format)"
   sensitive   = true
 }
 
 variable "tags" {
   type        = map(string)
-  description = "A map of tags to apply to the App Service Plan and Web App resources."
+  description = "Tags to apply to all Azure resources"
 }
