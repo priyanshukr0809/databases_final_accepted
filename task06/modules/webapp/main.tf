741ef415085d8981b39ed01242a85dabd5aadac7
PRIYANSHU KUMAR <priyanshu_kumar@epam.com>
2025-05-21 07:07:06 +0530

    Update main.tf

--- task06/modules/webapp/main.tf
+++ task06/modules/webapp/main.tf
@@ -1,17 +1,18 @@
 resource "azurerm_service_plan" "asp" {
   name                = var.asp_name
+  resource_group_name = var.resource_group_name
   location            = var.location
-  resource_group_name = var.rg_name
   os_type             = "Linux"
   sku_name            = var.asp_sku
   tags                = var.tags
 }
 
-resource "azurerm_linux_web_app" "webapp" {
+resource "azurerm_linux_web_app" "app" {
   name                = var.app_name
+  resource_group_name = var.resource_group_name
   location            = var.location
-  resource_group_name = var.rg_name
   service_plan_id     = azurerm_service_plan.asp.id
+  tags                = var.tags
 
   site_config {
     always_on = true
@@ -21,11 +22,8 @@ resource "azurerm_linux_web_app" "webapp" {
   }
 
   connection_string {
-    name  = "SqlConnectionString"
+    name  = "DefaultConnection"
     type  = "SQLAzure"
     value = var.sql_connection_string
-
   }
-
-  tags = var.tags
 }
