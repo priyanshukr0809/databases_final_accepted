ab85c5f8c9bd5b6f924e0abeca7f0683085d30a9
PRIYANSHU KUMAR <priyanshu_kumar@epam.com>
2025-05-21 07:19:06 +0530

    Update outputs.tf

--- task06/modules/webapp/outputs.tf
+++ task06/modules/webapp/outputs.tf
@@ -1,4 +1,4 @@
 output "app_hostname" {
   description = "Hostname of the Linux Web App"
-  value       = azurerm_linux_web_app.webapp.default_hostname
+  value       = azurerm_linux_web_app.app.default_hostname
 }
